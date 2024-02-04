e3693542390167ca8a995cfd262a7339e28e2e2f
Aman <ak193566@gmail.com>
2024-02-04 21:37:14 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,32 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // localhost
   // server:{
   //   proxy:{
   //     '/v2': 'https://www.reliancedigital.in/rildigitalws'
+  //   }
+  // },
+  // netlify
+  server: {
+    proxy: {
+      '/v2': {
+        target: 'https://www.reliancedigital.in',
+        pathRewrite: { '^/v2': '/rildigitalws' },
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  //vercel
+  // server: {
+  //   proxy: {
+  //     '/v2': {
+  //       target: 'https://www.reliancedigital.in/rildigitalws',
+  //       pathRewrite: { '^/v2': '' },
+  //       changeOrigin: true,
+  //       secure: false
+  //     }
   //   }
   // },
   plugins: [react()],
